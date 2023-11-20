3cd7a71950c30e7e471305030d538957b390f79c
tulpenkiste <tulpenkiste@amogus.cloud>
2023-11-20 16:46:50 +0100

    Remove unused boost imports

--- src/backend.cpp
+++ src/backend.cpp
@@ -10,8 +10,6 @@
 #include <QFile>
 #include <QMessageBox>
 
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/ini_parser.hpp>
 #include <vector>
 
 const std::vector<std::string> regions = {"us", "eu", "jp"};
